337617c3c55a44fe5006942b9ef0ecc3737a8911
AryaPathak <99078194+AryaPathak@users.noreply.github.com>
2023-06-08 22:11:49 +0530

    Addition list updated

--- src/Componants/ToDoReact/todo.js
+++ src/Componants/ToDoReact/todo.js
@@ -4,6 +4,16 @@ import "./style.css"
 const Todo = () => {
 
   const [inputdata, setInputData] = useState("");
+  const [items, setItems] = useState([]);
+
+  const addItem = () => {
+    if(!inputdata){
+      alert("Please fill the data");
+    }else{
+      setItems([...items,inputdata])
+      setInputData("");
+    }
+  }
 
   return (
     <>
@@ -18,20 +28,26 @@ const Todo = () => {
             <input 
               type="text" 
               placeholder="✍ Add Item" 
-              className="form-control"/>
-              value={}
-              onChange={}
-            <i className="fa fa-plus add-btn"></i>
+              className="form-control"
+              value={inputdata}
+              onChange={(event)=>setInputData(event.target.value)}
+              />
+            <i className="fa fa-plus add-btn" onClick={addItem}></i>
         </div>
 
         <div className="showItem">
-          <div className="eachItem">
-            <h3>Apple</h3>
-            <div className="todo-btn">
-              <i className="far fa-edit add-btn"></i>
-              <i className="far fa-trash-alt add-btn"></i>
+          {items.map((currElem, index) =>{
+            return (
+              <div className="eachItem" key={index}>
+              <h3>{currElem}</h3>
+              <div className="todo-btn">
+                <i className="far fa-edit add-btn"></i>
+                <i className="far fa-trash-alt add-btn"></i>
+              </div>
             </div>
-          </div>
+            );
+          })}
+          
         </div>
 
         <div className="showItems">
